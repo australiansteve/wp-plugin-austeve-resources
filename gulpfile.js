ac523c6722e1de83ff3aa63fde60d3ef7d75a92f
Steve <steve@australiansteve.com>
2017-12-19 23:25:51 -0400

    Initial commit of v2 Events CPT plugin, that contains no layout or styling, purely functionality

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+// Load our plugins
+var	gulp			=	require('gulp');
+
+var paths = {
+	sassPath: 'sass/',
+	destPath: '.'
+};
+
+//Our 'deploy' task which deploys on a local dev environment
+gulp.task('deploy', function() {
+
+	var files = ['images/**.*',
+		'*.php',
+		'*.css',
+		'js/*.js'];
+
+	var destSSJ = '/Applications/MAMP/htdocs/ssj/wp-content/plugins/austeve-display-posts';
+
+	return gulp.src(files, {base:"."})
+    		.pipe(gulp.dest(destSSJ));
+});
+
+// Our default gulp task, which runs all of our tasks upon typing in 'gulp' in Terminal
+gulp.task('default', ['deploy']);
